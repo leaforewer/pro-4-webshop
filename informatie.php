f5272a759cbb71c0c8a51c46431364a23088e398
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2020-06-04 10:17:15 +0200

    Informatie.php

---
+++ informatie.php
@@ -0,0 +1,138 @@
+<link rel="stylesheet" href="./css/style.css">
+
+<div class="jumbotron" id="jumbotron1">
+    <h1 class="display-4" id="Jumbotron_title" style="text-align:center;">Informatie</h1>
+    <hr class="my-4">
+    <h3 id="jumbotron_text" style="text-align:center;">Op deze pagina kunt u allerlei informatie vinden over winkel, reparatie, 
+    FAQ en adviezen over smartwatches.</h3>
+    <p class="lead">
+    </p>
+</div>
+
+<div id="accordion">
+    <!-- eerste tab -->
+    <div class="card">
+        <div class="card-header" id="headingOne" >
+            <h5 class="mb-0">
+                <button class="btn" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne" id="card_collapse_text">
+                    E-watch 
+                </button>
+            </h5>
+        </div>
+        <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+            <div class="card-body">
+                De afkorting ADHD staat voor Attention Deficit Hyperactivity Disorder.
+                In het Nederlands betekent dat aandachtstekortstoornis met hyperactiviteit.
+                De hersenen van mensen met ADHD reageren sneller op prikkels (impulsen, stemmingen en gebeurtenissen).
+                Dit kan ertoe leiden dat je snel bent afgeleid en je je moeilijk kunt concentreren.
+            </div>
+        </div>
+    </div>
+    <!-- tweede tab -->
+    <div class="card">
+        <div class="card-header" id="headingTwo">
+            <h5 class="mb-0">
+                <button class="btn collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo" id="card_collapse_text">
+                    Winkel
+                </button>
+            </h5>
+        </div>
+        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+            <div class="card-body">
+                De betekenis van ADHD is Attention Deficit Hyperactivity Disorder.
+                Het is een afwijking van kleine onderdelen van de hersenen.
+                De hersenen van kinderen en volwassenen met ADHD reageren sneller op prikkels.
+                Dit kan er toe leiden dat je je moeilijk kunt concentreren en snel afgeleid raakt (aandachtsproblemen).
+                Je kunt ook impulsief gedrag vertonen – bijvoorbeeld dingen doen zonder er eerst goed over na te denken.
+                Vaak ben je lichamelijk overactief of je voelt je altijd onrustig (hyperactiviteit).
+                ADHD wordt nog wel eens verward met ADD, een subtype van ADHD.
+            </div>
+        </div>
+    </div>
+    <!-- derde tab -->
+    <div class="card">
+        <div class="card-header" id="headingThree">
+            <h5 class="mb-0">
+                <button class="btn collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree " id="card_collapse_text">
+                    Reparatie
+                </button>
+            </h5>
+        </div>
+        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+            <div class="card-body">
+                <p>
+                    Hoe ADHD bij een kind naar voren komt is niet vantevoren aan te geven.
+                    Net als ieder kind uniek is, is ook ieder kind met ADHD uniek.
+                    Ook gedrag is verschillend.
+                    Kinderen met ADHD zijn niet altijd druk, ze zijn niet allemaal impulsief en ze zijn ook niet allemaal ongeconcentreerd.
+                    <br>
+                    <br>
+
+                    De belangrijkste kenmerken van ADHD bij volwassenen zijn:
+                    problemen met de aandacht of de concentratie, moeite met organiseren,
+                    hyperactief of overbeweeglijk zijn en impulsiviteit.
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <!-- vierde tab -->
+    <div class="card">
+        <div class="card-header" id="headingFour">
+            <h5 class="mb-0">
+                <button class="btn collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour" id="card_collapse_text">
+                    FAQ
+                </button>
+            </h5>
+        </div>
+        <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+            <div class="card-body">
+                Je hoeft niet alle kenmerken te herkennen om de diagnose ADHD te krijgen.
+                De kenmerken van ADHD zijn te verdelen in 3 gebieden: aandachtsproblemen, hyperactiviteit en impulsiviteit.
+                <br>
+                <br>
+                Onder aandachtsproblemen verstaan we problemen met de concentratie en het kunnen organiseren.
+                Veel ADHD'ers zijn snel afgeleid , hebben moeite met luisteren en zijn chaotisch.
+                Het afmaken van opdrachten, het scheiden van hoofd- en bijzaak, plannen,
+                organiseren en het doen van administratieve taken is dan ook vaak een uitdaging.
+                <br>
+                <br>
+                Ook hyperactiviteit is een veelvoorkomend kenmerk van ADHD.
+                Veel volwassenen met ADHD hebben moeite met stil zitten, zijn rusteloos,
+                willen graag altijd bezig zijnen hebben moeite met ontspannen.
+                <br>
+                <br>
+                Het laatste gebied is impulsiviteit.
+                Mensen met ADHD doen eerst voor ze nadenken over de gevolgen,
+                zo kunnen ze er iets zomaar uit flappen en worden beslissingen soms te snel genomen.
+                Een andere uiting van impulsiviteit is het moeilijk vinden om op de beurt te wachten en het door andere
+                mensen heen praten. Impulsiviteit ontstaat doordat de (vaak onbewuste) remfunctie die het gedrag regelt
+                onvoldoende ontwikkeld is.
+                <br>
+                <br>
+                ADHD kent natuurlijk ook positieve kanten.
+                Zo zijn mensen met ADHD vaak creatief, kunnen ze goed problemen oplossen en zijn ze origineel in hun doen en laten.
+            </div>
+        </div>
+    </div>
+    <!-- vijfde tab -->
+    <div class="card">
+        <div class="card-header" id="headingFive">
+            <h5 class="mb-0">
+                <button class="btn collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive" id="card_collapse_text">
+                    Adviezen
+                </button>
+            </h5>
+        </div>
+        <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+            <div class="card-body">
+                ADHD is meestal aangeboren. Dit wil niet zeggen dat een kind geboren wordt met ADHD, maar wel met de aanleg voor ADHD.
+                De aandoening is voor deel erfelijk.
+                Als je ouders of eventuele broers of zussen ook ADHD hebben is er een grote kans dat je zelf ook ADHD hebt.
+                De diagnose ADHD wordt vaak al voor het zevende levensjaar gesteld.
+                In Nederland heeft ongeveer 3 op de 100 mensen ADHD of ADD.
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
