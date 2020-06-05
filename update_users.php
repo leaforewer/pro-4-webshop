dceee80d7cd1da1ed2ed7ae01d95e23e9e9d2d7a
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-06-05 17:51:48 +0200

    update script gefixed

--- update_users.php
+++ update_users.php
@@ -15,33 +15,40 @@ echo "</pre>";
 ?>
 
 <body style="background-color: gray;">
-<div class="row">
-        <div class="col-12">
+    <div class="row">
+        <div class="col-4 offset-4">
             <h3 style="padding-top: 30px;">Verander User Email & Role</h3>
-            
-            <br/><br/>
+
+            <br /><br />
         </div>
-        <div style=" padding-top: 30px;background-color: #262935;" class="col-6 abc">
+        <div style="padding-top: 30px;background-color: #262935;" class="col-4 offset-4 abc">
             <form action="./index.php?content=update_script_users" method="post">
                 <div class="form-group">
                     <label style="color: whitesmoke;" for="infix">E-mailadres</label>
                     <input type="text" class="form-control" id="email" aria-describedby="emailHelp" name="email" value="<?php echo $record["email"]; ?>">
-                    <small id="Help" class="form-text ">*Verplicht veld</small>
+                    <small style="padding-top: 10px;" id="Help" class="form-text ">*Verplicht veld</small>
                 </div>
                 <div class="form-group">
-                <label  style=" color: whitesmoke; padding-right: 350px; padding-bottom: 5px;" for="userrole">UserRole</label>
-                <select id="userrole" name="userrole" style="padding-left: 5px; padding-top: 2px; padding-bottom: 2px;">
-                    <option value='customer'>Customer</option>
-                    <option value='admin'>Admin</option>
-                    <option value='root'>Root</option>
-                </select>
-                <small id="Help" class="form-text ">*Selecteer role</small>
-            </div>
-                <div class="col-12" style="padding-left: 375px; padding-top: 40px; padding-bottom: 40px;">
+                    <input type="hidden" class="form-control disabled" id="password" aria-describedby="passwordHelp" name="password" value="<?php echo $record["password"]; ?>">
+                </div>
+                <div class="form-group">
+                    <input type="hidden" id="handteken" aria-describedby="handtekenHelp" name="handteken" value="<?php echo $record["handteken"]; ?>">
+                </div>
+                <div class="form-group">
+                    <label style=" color: whitesmoke;" for="userrole">UserRole</label><br />
+                    <select id="userrole" name="userrole" style="padding-left: 5px; padding-top: 2px; padding-bottom: 2px;">
+                        <option value='customer'>Customer</option>
+                        <option value='admin'>Admin</option>
+                        <option value='root'>Root</option>
+                    </select>
+                    <small style="padding-top: 10px;" id="Help" class="form-text ">*Selecteer role</small>
+                </div>
+                <div class="col-12" style="padding-left: 475px; padding-top: 40px; padding-bottom: 40px;">
+                    <input type="hidden" value="<?php echo $record["activated"]; ?>" name="activated">
                     <input type="hidden" value="<?php echo $id; ?>" name="id">
                     <button type="submit" class="btn btn-warning ">Submit</button>
                 </div>
             </form>
-        
-    </div>
+
+        </div>
 </body>
