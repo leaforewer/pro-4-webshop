fa260fda92b18b90716163235fd5a23e336dd63f
tyrapeut <56019575+tyrapeut@users.noreply.github.com>
2020-06-06 14:10:23 +0200

    navbar rightside fixes

--- navbar.php
+++ navbar.php
@@ -53,14 +53,26 @@
   </div>
    
   
-  <ul class="nav navbar-nav navbar-right">
-        <li>
-            <a href="#"> Sign Up</a>
-        </li>
-        <li>
-            <a href="#">Login</a>
-        </li>
-    </ul>     
+  <div class="navbar">
+    <ul class="nav navbar-nav navbar-right mr-auto lg-0">
+            <li class="nav-item <?php if ($active == "home") {echo "active";} ?>">
+                <a class="nav-link" href="index.php?content=register" id="navText"> 
+                    <svg class="bi bi-box-arrow-in-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M8.146 11.354a.5.5 0 0 1 0-.708L10.793 8 8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0z"/>
+                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 1 8z"/>
+                        <path fill-rule="evenodd" d="M13.5 14.5A1.5 1.5 0 0 0 15 13V3a1.5 1.5 0 0 0-1.5-1.5h-8A1.5 1.5 0 0 0 4 3v1.5a.5.5 0 0 0 1 0V3a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v10a.5.5 0 0 1-.5.5h-8A.5.5 0 0 1 5 13v-1.5a.5.5 0 0 0-1 0V13a1.5 1.5 0 0 0 1.5 1.5h8z"/>
+                    </svg>Reigistreren
+                </a>
+            </li>
+            <li class="nav-item <?php if ($active == "home") {echo "active";} ?>">
+                <a class="nav-link" href="./index.php?content=login" id="navText">
+                    <svg class="bi bi-person" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M13 14s1 0 1-1-1-4-6-4-6 3-6 4 1 1 1 1h10zm-9.995-.944v-.002.002zM3.022 13h9.956a.274.274 0 0 0 .014-.002l.008-.002c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664a1.05 1.05 0 0 0 .022.004zm9.974.056v-.002.002zM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                    </svg>Inloggen
+                </a>
+            </li>
+        </ul>     
+  </div>
 
   
 </nav>
