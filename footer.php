b003cfee99f4b5b050804075e13f41d20774579a
tyrapeut <56019575+tyrapeut@users.noreply.github.com>
2020-06-07 13:31:31 +0200

    footer setup

---
+++ footer.php
@@ -0,0 +1,38 @@
+    <div >
+        <div class="row" >
+            <div class="col-3">                
+                <h5 id="footerText1">Informatie</h5>
+                <a href="./index.php?content=informatie">
+                    <p>FAQ's</p>
+                </a>
+                <a href="./index.php?content=contact">
+                    <p>Contact</p>
+                </a>                       
+            </div>
+            <div class="col-3">                
+                <h5 id="footerText1">Account</h5>               
+                    <a href="./index.php?content=login">
+                        <p>Inloggen</p>
+                    </a>
+                    <a href="./index.php?content=register">
+                        <p>Registreren</p>
+                    </a>
+            </div>
+            <div class="col-3">               
+                <h5 id="footerText1">Producten</h5>                
+                    <a href="./index.php?content=ProductsApple">
+                        <p>Apple</p>                    
+                    </a>
+                    <a href="./index.php?content=ProductsSamsung">
+                        <p>Samsung</p>
+                    </a>
+                    <a href="./index.php?content=ProductsGarmin">
+                        <p>Garmin</p>                    
+                    </a>
+                    <a href="./index.php?content=ProductsHuawei">
+                        <p>Huawei</p>                   
+                    </a>
+            </div>
+            <div class="col-3"></div>
+        </div>
+    </div>
