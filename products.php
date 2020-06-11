969b8c22a033d2a34e84c4bbaf2829d25614672f
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-06-11 11:28:03 +0200

    products ingevoegd

---
+++ products.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Watch</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- FONTS -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/navbar.css">
+</head>
+<div class="col-12">
+    <div class="card-deck">
+        <div>
+            <img src="./img/watch_1.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 130 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img src="./img/watch_2.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 175 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <img src="./img/watch_3.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 159 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</html>
