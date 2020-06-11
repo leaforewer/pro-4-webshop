1b32160a866b82525cf14b26632a3321c5e7e214
Talha Berk Kiymaz <326753@student.mboutrecht.nl>
2020-06-11 11:55:53 +0200

    products

--- products.php
+++ products.php
@@ -1,34 +1,56 @@
-<div class="col-2"></div>
-<div class="row" id="products">
-    
-    <div class="col-3">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="col-12">
+    <div class="card-deck">
+        <div>
+            <img src="./img/watch_1.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 130 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-3">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div>
+            <img src="./img/watch_2.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 175 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="col-3">
-        <div class="card">
-            <img src="..." class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+        <div>
+            <img src="./img/watch_3.jpg" class="card-img-top" alt="...">
+            <div class="card border-info mb-3" style="max-width: 18rem;">
+                <div class="card-body text-dark">
+                    <h5 class="card-title">Watch 1</h5>
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 159 -</p>
+                        <div style="padding-left: 180px;">
+                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
+                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-</div>
