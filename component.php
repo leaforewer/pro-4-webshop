ba4e34b49ebb369ee7f67f1549e33fe49a349fa5
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-06-16 14:15:45 +0200

    products zijn gepast aan php deel 1

---
+++ component.php
@@ -0,0 +1,25 @@
+<?php
+
+function component($productname, $productprice, $productimg)
+{
+    $element = "
+    
+<form action='products.php' method='post'>
+        <div>
+            <img id='img-product' src='$productimg' class='card-img-top' alt='...'>
+            <div class='card border-info mb-3' style='max-width: 18rem;'>
+                <div class='card-body text-dark'>
+                    <h5 class='card-title'>$productname</h5>
+                    <p class='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <div>
+                        <span style='font-size: 1.5em; color: red; padding-right: 1em;'>$productprice € -</span>
+                            <button type='submit' class='btn btn-warning my-3' name='add'>Voeg in winkelwagen <ion-icon style='width: 22px; height:22px;' name='cart'></ion-icon></button>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+</form>        
+";
+    echo $element;
+}
--- products.php
+++ products.php
@@ -1,56 +1,17 @@
+<?php
+
+require_once('./component.php');
+
+?>
 <div class="col-12">
     <div class="card-deck">
-        <div>
-            <img src="./img/watch_1.jpg" class="card-img-top" alt="...">
-            <div class="card border-info mb-3" style="max-width: 18rem;">
-                <div class="card-body text-dark">
-                    <h5 class="card-title">Watch 1</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <div>
-                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 130 -</p>
-                        <div style="padding-left: 180px;">
-                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
-                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
-                        </div>
+        <?php
 
+        component($productname="Product 1", $productprice= 139, $productimg= "./img/watch_1.jpg");
+        component($productname="Product 2", $productprice= 89, $productimg= "./img/watch_2.png");
+        component($productname="Product 3", $productprice= 189, $productimg= "./img/watch_3.png");
 
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <img src="./img/watch_2.jpg" class="card-img-top" alt="...">
-            <div class="card border-info mb-3" style="max-width: 18rem;">
-                <div class="card-body text-dark">
-                    <h5 class="card-title">Watch 1</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <div>
-                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 175 -</p>
-                        <div style="padding-left: 180px;">
-                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
-                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
-                        </div>
+        ?>
 
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <img src="./img/watch_3.jpg" class="card-img-top" alt="...">
-            <div class="card border-info mb-3" style="max-width: 18rem;">
-                <div class="card-body text-dark">
-                    <h5 class="card-title">Watch 1</h5>
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                    <div>
-                        <p style="font-size: 1.5em; color: red; padding-right: 1em;">€ 159 -</p>
-                        <div style="padding-left: 180px;">
-                            <a style=" background-color: #0EBFE9;border-radius: 15px; padding: 0.5em;" href="">
-                                <img src="./img/cart.png" style="max-width: 40px; max-height:35px;" alt=""></a>
-                        </div>
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+</div>
