392fe1294a60794c93dfe0bc86e582fc1f905200
Ahmet Erdogan <329188@student.mboutrecht.nl>
2020-06-18 09:14:05 +0200

    products deel 2

--- component.php
+++ component.php
@@ -1,10 +1,10 @@
 <?php
 
-function component($productname, $productprice, $productimg)
+function component($productname, $productprice, $productimg, $productid)
 {
     $element = "
     
-<form action='products.php' method='post'>
+<form action='./index.php?content=products' method='POST'>
         <div>
             <img id='img-product' src='$productimg' class='card-img-top' alt='...'>
             <div class='card border-info mb-3' style='max-width: 18rem;'>
@@ -13,8 +13,8 @@ function component($productname, $productprice, $productimg)
                     <p class='card-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                     <div>
                         <span style='font-size: 1.5em; color: red; padding-right: 1em;'>$productprice € -</span>
+                            <button type='hidden' style='display: none;' name='productid' value='$productid'></button>
                             <button type='submit' class='btn btn-warning my-3' name='add'>Voeg in winkelwagen <ion-icon style='width: 22px; height:22px;' name='cart'></ion-icon></button>
-                        
                     </div>
                 </div>
             </div>
--- products.php
+++ products.php
@@ -1,16 +1,19 @@
 <?php
 
+include('./connect_db.php');
 require_once('./component.php');
 
+if(isset($_POST['add'])){
+
+}
 ?>
 <div class="col-12">
     <div class="card-deck">
         <?php
 
-        component($productname="Product 1", $productprice= 139, $productimg= "./img/watch_1.jpg");
-        component($productname="Product 2", $productprice= 89, $productimg= "./img/watch_2.png");
-        component($productname="Product 3", $productprice= 189, $productimg= "./img/watch_3.png");
-
+        component($productname="Garmin VivoActive", $productprice= 139, $productimg= "./img/watch_1.jpg", $productid=1);
+        component($productname="Suunto 5", $productprice= 89, $productimg= "./img/watch_2.png", $productid=2);
+        component($productname="Diesel Classic", $productprice= 189, $productimg= "./img/watch_3.png", $productid=3);
         ?>
 
     </div>
