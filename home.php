692733a605fee98f785e7c53bfc237ad9d1ca6d2
Tyra Afoley Okai Cudjoe <tyra20015@gmail.com>
2020-06-23 13:46:49 +0200

    repositionering van jumbotron

--- home.php
+++ home.php
@@ -13,28 +13,31 @@
 
 
 
-
-<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="./img/watch.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/smart1.jpg" class="d-block w-100" alt="...">
-    </div>
-    <div class="carousel-item">
-      <img src="./img/smart2.jpg" class="d-block w-100" alt="...">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
+<div class="row">
+    <div class="col-1"></div>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade col-10" data-ride="carousel">
+      <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="./img/watch.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/smart1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="./img/smart2.jpg" class="d-block w-100" alt="...">
+          </div>
+       </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+   </div>
 </div>
+
 
 <div class="row" id="cardsHome">
     <div class="col-4">
